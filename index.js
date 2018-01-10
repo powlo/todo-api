8e42f6602aca7b85e4eb1c60199b3c9bf48e0559
Paul Backhouse <paulcbackhouse@gmail.com>
2018-01-10 17:33:47 +0000

    Added frontend static index.

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/api/todos', todoRoutes);
 
+//theres no need to do this...
 app.get('/', function(req, res){
-  res.send('Hi');  
+  res.sendFile('index.html');
 });
 
 app.listen(port, function(){
